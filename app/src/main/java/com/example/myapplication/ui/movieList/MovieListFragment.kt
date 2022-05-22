49ad56cf3f33081b2b39239fa0b8c5f5deeebf9c
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-05-22 08:06:55 +0430

    fea/create nav graph

--- app/src/main/java/com/example/myapplication/ui/movieList/MovieListFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/movieList/MovieListFragment.kt
@@ -24,7 +24,7 @@ class MovieListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_movie_list,container,false)
