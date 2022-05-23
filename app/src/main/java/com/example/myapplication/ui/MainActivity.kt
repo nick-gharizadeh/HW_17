43ab04926d1a85e090ae4a5249506f0d0c0f6cb6
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-05-23 10:48:14 +0430

    fea/create search menu

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.item_search -> {
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
