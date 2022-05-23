9c710bf8b7ff0fa582fa78fcd31d87a53e776ce3
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-05-23 10:55:20 +0430

    fea/create transaction for go to search fragment

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +21,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.item_search -> {
-
+                val fragment: Fragment = SearchFragment()
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragmentContainerView, fragment)
+                transaction.addToBackStack("")
+                transaction.commit()
             }
 
         }
